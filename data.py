c75645636cdf202578cd81328bbd873c9c1a301b
lanhai <yuri924@163.com>
2025-06-16 19:32:45 +0800

    20250616

--- data.py
+++ data.py
@@ -1,13 +1,16 @@
 import os.path as osp
 import ezc3d
 from glob import glob
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
+from marker_vids import all_marker_vids, general_labels_map
+import re
 
 class MocapSequenceDataset(Dataset):
     def __init__(self, sequence_dir):
         self.sequence_paths =  glob(osp.join(sequence_dir, '*/*.c3d'))  # or .pkl/.npz
-        self.sequence_labels = ['_'.join(a.split('/')[-3:-1]) for a in self.sequence_paths]
-        self.subject_names = [a.split('/')[-2] for a in self.sequence_paths]   
+        self.sequence_labels = ['_'.join(re.split(r'[/.]',a)[-4:-1]) for a in self.sequence_paths]
+        self.subject_names = [a.split('/')[-2] for a in self.sequence_paths]
+        self.smpl_id_tab  = all_marker_vids['smpl']
     
     def __len__(self):
         return len(self.sequence_paths)
@@ -18,12 +21,43 @@ class MocapSequenceDataset(Dataset):
         markers = marker_data['data']['points'][:3].transpose(2, 1, 0)
         frame_rate = marker_data['parameters']['POINT']['RATE']['value'][0]
         labels = marker_data['parameters']['POINT']['LABELS']['value']
+        labels = [item.split(':')[-1] for item in labels]
+        general_labels = [key for key in general_labels_map]
+        mapped_labels = [general_labels_map[key] if key in general_labels else key for key in labels]
+        smplid = [self.smpl_id_tab[key] for key in mapped_labels]
 
         return {
             "sequence_labels": self.sequence_labels[idx],
             'subject_name': self.subject_names[idx],
             'markers': markers, 
-            'labels': labels, 
+            'labels': labels,
+            'id': smplid,
             'frame_rate': frame_rate, 
             'marker_data': marker_data
         }
+
+class MocapDataLoader(DataLoader):
+    def __init__(self, dataset, stage='i', sample_size=12, **kwargs):
+        self.stage = stage
+        self.sample_size = sample_size
+        self.dataset = dataset
+        super().__init__(dataset, **kwargs)
+
+    def __iter__(self):
+        if self.stage == 'i':
+            # Stage I: 从所有帧中采样
+            indices = self.dataset.sample_indices(self.sample_size)
+        elif self.stage == 'ii':
+            # Stage II: 每次以一个 c3d 文件为单位
+            pass
+        else:
+            raise ValueError(f"Unknown stage {self.stage}")
+
+        # 用 PyTorch 的 Sampler 和 batch 机制处理这些 index
+        sampler = torch.utils.data.SequentialSampler(indices)
+        batch_sampler = torch.utils.data.BatchSampler(sampler, batch_size=self.batch_size, drop_last=False)
+
+        # 复用 PyTorch 的 DataLoader 工作线程、collate_fn 等机制
+        return super().__iter__().__class__(self.dataset, batch_sampler=batch_sampler,
+                                            collate_fn=self.collate_fn, num_workers=self.num_workers,
+                                            pin_memory=self.pin_memory)
--- moshpp_torch.py
+++ moshpp_torch.py
@@ -4,7 +4,9 @@ from data import MocapSequenceDataset
 import os.path as osp
 from loguru import logger
 from omegaconf import OmegaConf
+from typing import OrderedDict as OrderedDictType
 from smpl import Smpl
+import random
 class moshpp(torch.nn.Module):
     def __init__(self):
         super().__init__()
@@ -28,71 +30,67 @@ class moshpp(torch.nn.Module):
             logger.info('Setting moshpp.optimize_face to False')
             cfg.moshpp.optimize_face = False
 
-    logger.info(f'Loading model: {surface_model_fname}')
+        logger.info(f'Loading model: {surface_model_fname}')
 
-    if surface_model_type == 'object':
-        from moshpp.models.object_model import RigidObjectModel
-        can_model = RigidObjectModel(ply_fname=surface_model_fname)
-        beta_shared_models = [RigidObjectModel(ply_fname=surface_model_fname) for _ in
-                                               range(num_beta_shared_models)]
-    else:
-        from moshpp.prior.gmm_prior_ch import create_gmm_body_prior
-
-        sm_temp = load_surface_model(surface_model_fname=surface_model_fname,
-                                     pose_hand_prior_fname=pose_hand_prior_fname,
-                                     use_hands_mean=use_hands_mean,
-                                     dof_per_hand=dof_per_hand,
-                                     v_template_fname=v_template_fname,
-                                     surface_model_type=surface_model_type,
-                                     #this is to address the models that have the same number of jonts i.e. dog, rat
-                                     )
-
-        betas = ch.array(np.zeros(len(sm_temp.betas)))
-
-        can_model = Smplx(trans=ch.array(np.zeros(sm_temp.trans.size)),
-                                 pose=ch.array(np.zeros(sm_temp.pose.size)),
-                                 betas=betas,
-                                 temp_model=sm_temp)
-
-        assert can_model.model_type == surface_model_type, ValueError(f'{can_model.model_type} != {surface_model_type}')
-        priors = {}
-        if surface_model_type == 'animal_horse':
-            from moshpp.prior.horse_body_prior import smal_horse_prior, smal_horse_joint_angle_prior
-            if pose_body_prior_fname:
-                priors['pose'] = smal_horse_prior(pose_body_prior_fname)
-            priors['pose_jangles'] = smal_horse_joint_angle_prior()
-        elif surface_model_type == 'animal_dog':
-            from moshpp.prior.dog_body_prior import MaxMixtureDog
-            # from moshpp.prior.smal_dog_prior import smal_dog_prior, smal_dog_joint_angle_prior
-            if pose_body_prior_fname:
-                priors['pose'] = MaxMixtureDog(prior_pklpath=pose_body_prior_fname).get_gmm_prior()
-            # priors['pose_jangles'] = smal_dog_joint_angle_prior()
+        if surface_model_type == 'object':
+            from moshpp.models.object_model import RigidObjectModel
+            can_model = RigidObjectModel(ply_fname=surface_model_fname)
+            beta_shared_models = [RigidObjectModel(ply_fname=surface_model_fname) for _ in
+                                                   range(num_beta_shared_models)]
         else:
-            if pose_body_prior_fname:
-                priors['pose'] = create_gmm_body_prior(pose_body_prior_fname=pose_body_prior_fname,
-                                                   exclude_hands=surface_model_type in ['smplh', 'smplx'])
-        if not optimize_face:
-            priors['betas'] = AliasedBetas(sv=can_model, surface_model_type=surface_model_type)
+            from moshpp.prior.gmm_prior_ch import create_gmm_body_prior
 
-        can_model.priors = priors
+            sm_temp = load_surface_model(surface_model_fname=surface_model_fname,
+                                         pose_hand_prior_fname=pose_hand_prior_fname,
+                                         use_hands_mean=use_hands_mean,
+                                         dof_per_hand=dof_per_hand,
+                                         v_template_fname=v_template_fname,
+                                         surface_model_type=surface_model_type,
+                                         #this is to address the models that have the same number of jonts i.e. dog, rat
+                                         )
 
-        # do not share betas when optimizing betas to go around an issue of chumpy
-        # with it current implementation of SMPL-X body shape betas and facial expressions share the same parameter; i.e. betas
-        # now mosh requires betas to be shared across the canonical model and the 12 frames
-        # now those 12 frames cannot share expressions though!
-        # and that requires double indexing the same array; i.e. once for body shape and once for expressions
-        # that messes up chumpy since it holds a grudge on double indexing the same optimization free variable
-        # solution is to precompute betas and to not lock it at all when face needs to be optimized
-        beta_shared_models = [SmplModelLBS(pose=ch.array(np.zeros(can_model.pose.size)),
-                                           trans=ch.array(np.zeros(can_model.trans.size)),
-                                           betas=can_model.betas if not optimize_face else ch.array(np.zeros(can_model.betas.size)),
-                                           temp_model=can_model) for _ in range(num_beta_shared_models)]
+            betas = ch.array(np.zeros(len(sm_temp.betas)))
 
-    return can_model, beta_shared_models
+            can_model = Smplx(trans=ch.array(np.zeros(sm_temp.trans.size)),
+                                     pose=ch.array(np.zeros(sm_temp.pose.size)),
+                                     betas=betas,
+                                     temp_model=sm_temp)
 
+            assert can_model.model_type == surface_model_type, ValueError(f'{can_model.model_type} != {surface_model_type}')
+            priors = {}
+            if surface_model_type == 'animal_horse':
+                from moshpp.prior.horse_body_prior import smal_horse_prior, smal_horse_joint_angle_prior
+                if pose_body_prior_fname:
+                    priors['pose'] = smal_horse_prior(pose_body_prior_fname)
+                priors['pose_jangles'] = smal_horse_joint_angle_prior()
+            elif surface_model_type == 'animal_dog':
+                from moshpp.prior.dog_body_prior import MaxMixtureDog
+                # from moshpp.prior.smal_dog_prior import smal_dog_prior, smal_dog_joint_angle_prior
+                if pose_body_prior_fname:
+                    priors['pose'] = MaxMixtureDog(prior_pklpath=pose_body_prior_fname).get_gmm_prior()
+                # priors['pose_jangles'] = smal_dog_joint_angle_prior()
+            else:
+                if pose_body_prior_fname:
+                    priors['pose'] = create_gmm_body_prior(pose_body_prior_fname=pose_body_prior_fname,
+                                                       exclude_hands=surface_model_type in ['smplh', 'smplx'])
+            if not optimize_face:
+                priors['betas'] = AliasedBetas(sv=can_model, surface_model_type=surface_model_type)
 
+            can_model.priors = priors
 
+            # do not share betas when optimizing betas to go around an issue of chumpy
+            # with it current implementation of SMPL-X body shape betas and facial expressions share the same parameter; i.e. betas
+            # now mosh requires betas to be shared across the canonical model and the 12 frames
+            # now those 12 frames cannot share expressions though!
+            # and that requires double indexing the same array; i.e. once for body shape and once for expressions
+            # that messes up chumpy since it holds a grudge on double indexing the same optimization free variable
+            # solution is to precompute betas and to not lock it at all when face needs to be optimized
+            beta_shared_models = [SmplModelLBS(pose=ch.array(np.zeros(can_model.pose.size)),
+                                               trans=ch.array(np.zeros(can_model.trans.size)),
+                                               betas=can_model.betas if not optimize_face else ch.array(np.zeros(can_model.betas.size)),
+                                               temp_model=can_model) for _ in range(num_beta_shared_models)]
 
+        return can_model, beta_shared_models
 
 
     def forward(self, x: torch.Tensor):
@@ -111,6 +109,7 @@ class moshpp(torch.nn.Module):
 
 class StageOneFitter(torch.nn.Module):
     def __init__(self,
+                 num_markers = 46,
                  num_frames = 12,
                  num_betas=10,
                  device='cuda'):
@@ -122,30 +121,30 @@ class StageOneFitter(torch.nn.Module):
         tpose = torch.zeros([num_frames,23*3])
         self.register_buffer('tpose', tpose)
         self.device = device
+        marker_pose = torch.zeros()
+        self.marker_pose = torch.nn.Parameter()
 
-    def fit(self, train_loader, num_epochs=10):
+    def fit(self,
+            train_data,
+            num_epochs=10):
         print(f"Starting Stage i training for {num_epochs} epochs.")
-
-
 
         opt = torch.optim.LBFGS(parameters,
                                 max_iter=maxiter,
                                 line_search_fn='strong_wolfe')
+        self.model.train()
+
         for epoch in range(num_epochs):
-            self.train_one_epoch(train_loader, epoch)
-            if val_loader is not None:
-                self.validate(val_loader, epoch)
+
             if self.scheduler:
                 self.scheduler.step()
+            loss = self.model.training_step(batch)
 
-    def train_one_epoch(self, dataloader, epoch, stage):
-        self.model.train()
-        for batch in dataloader:
-            batch = {k: v.to(self.device) for k, v in batch.items()}
-            loss = self.model.training_step(batch, stage=stage)
-            self.optimizer.zero_grad()
+            opt.zero_grad()
             loss.backward()
-            self.optimizer.step()
+            opt.step()
+
+
 
 
 class StageTwoFitter:
@@ -188,12 +187,33 @@ def main():
     cfg = OmegaConf.load('./config.yaml')
 
     dataset = MocapSequenceDataset(cfg.paths.dataset_dir)
+    subjects = set(dataset.subject_names)
+    subject_ids = {s:[i for i, n in enumerate(dataset.subject_names) if n == s] for s in subjects}
 
 
-    # 2. load model
-    stageonefitter = StageOneFitter()
+    for ind in range(len(dataset)):
+        stage2_data = dataset[ind]
+        # prepare data for stage 1 training
+        ids = subject_ids[stage2_data['subject_name']]
+        random.shuffle(ids)
+        stage1_data = []
+        for i in ids:
+            data = dataset[i]
+            frame_ind = np.random.randint(0, data['markers'].shape[0], 3)
+            stage1_data.append({'data': data['markers'][frame_ind],
+                                'label':data['sequence_labels'],
+                                'id':data['id']})
+            if len(stage1_data)>=(12/3):
+                break
 
-    stageonefitter.fit()
+
+
+
+
+        # 2. load model
+        stageonefitter = StageOneFitter()
+
+        stageonefitter.fit()
 
     logger.debug('Final mosh stagei loss: {}'.format(' | '.join(
         [f'{k} = {np.sum(v ** 2):2.2e}' for k, v in mp.stagei_data['stagei_debug_details']['stagei_errs'].items()])))
